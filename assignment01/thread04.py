d2bb69b2c9e6533810574e37d6f9abbb171d7fb4
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-18 11:24:50 +0700

    Added comments to thread 4

--- assignment01/thread04.py
+++ assignment01/thread04.py
@@ -15,11 +15,11 @@ class CustomThread(Thread):
 
 # create the thread
 thread = CustomThread()
-
+# start thread
 thread.start()
-
+# Wait for it to finish
 print(f'{ctime()} Waiting for the thread to finish')
 thread.join()
-
+# Get the value from the thread
 value = thread.value
 print(f'{ctime()} Got: {value}')
