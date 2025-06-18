c7ef0f107685b19c3c2246b8cd256e30054ec5c6
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-18 11:50:15 +0700

    Wrote thread 01 of assignment 2

--- assignment02/thread01.py
+++ assignment02/thread01.py
@@ -1,1 +1,24 @@
 # Thread version of cooking 1 kitchen 1 chefs 1 dishes
+from time import time, ctime, sleep
+from threading import Thread
+
+def cooking(index):
+    start_time = time()
+    print(f'{ctime()} Kitchen Number {index}    : Has began cooking')
+    sleep(2)
+    duration = time() - start_time
+    print(f'{ctime()} Kitchen Number {index}    : Cooking finished in {duration:0.2f} seconds.')
+
+if __name__ == "__main__":
+# Begin threading
+    print(f'{ctime()} Main  : Starting cooking')
+    start_time = time()
+
+# Threading the cooking process
+    index=1
+    c1 = Thread(target=cooking(index))
+    c1.start()
+    c1.join()
+
+    duration = time() - start_time
+    print(f"{ctime()} Main  : Finished cooking in {duration:0.2f} seconds!")
