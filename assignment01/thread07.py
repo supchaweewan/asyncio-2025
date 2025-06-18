1a128fb94b0b352cbe76fa0d9c93daed28a0b5fb
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-18 11:27:03 +0700

    Added comments to thread 7

--- assignment01/thread07.py
+++ assignment01/thread07.py
@@ -14,3 +14,5 @@ if __name__ == "__main__":
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
         executor.map(thread_function, range(3))
+
+# the executor allows you to manage and join threads cleanly
