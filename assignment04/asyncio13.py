363d8b7cd93804234252935d121fd50a21f434e7
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:38:01 +0700

    completed asyncio13 assignment 4

--- assignment04/asyncio13.py
+++ assignment04/asyncio13.py
@@ -1,1 +1,17 @@
 # all tasks
+import asyncio
+
+async def dummy():
+    await asyncio.sleep(2)
+
+async def main():
+    t1 = asyncio.create_task(dummy(), name="Task A")
+    t2 = asyncio.create_task(dummy(), name="Task B")
+    await asyncio.sleep(0.1)
+
+    all_tasks = asyncio.all_tasks()
+    print("Tasks within the loop:")
+    for t in all_tasks:
+        print(" -", t.get_name())
+
+asyncio.run(main())
