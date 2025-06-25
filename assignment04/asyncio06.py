c4d5cb732e8cc62e07709e45280a9b6d990b4e09
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:37:04 +0700

    completed asyncio6 assignment 4

--- assignment04/asyncio06.py
+++ assignment04/asyncio06.py
@@ -1,1 +1,16 @@
 # check_cancelled.py
+import asyncio
+
+async def slow_task():
+    await asyncio.sleep(5)
+
+async def main():
+    task = asyncio.create_task(slow_task())
+    await asyncio.sleep(1)
+    task.cancel()
+    try:
+        await task
+    except asyncio.CancelledError:
+        print("Damn task was cancelled:", task.cancelled())
+
+asyncio.run(main())
