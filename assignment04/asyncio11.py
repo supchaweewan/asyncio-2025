08e67c341247463381e617ed88baff427104e5da
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:37:45 +0700

    completed asyncio11 assignment 4

--- assignment04/asyncio11.py
+++ assignment04/asyncio11.py
@@ -1,1 +1,12 @@
 # task name
+import asyncio
+
+async def simple_task():
+    await asyncio.sleep(1)
+
+async def main():
+    task = asyncio.create_task(simple_task(), name="Loading data.")
+    print("Task name: ", task.get_name())
+    await task
+
+asyncio.run(main())
