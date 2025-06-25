828aab9bfffe706d2b31a2e98e6232deed0cdd05
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:07:38 +0700

    completed asyncio03 assignment 4

--- assignment04/asyncio03.py
+++ assignment04/asyncio03.py
@@ -1,2 +1,16 @@
 # Create 2 Tasks with High-Level API
+import asyncio
 
+async def get_image(name, delay):
+    print(f"{name} is being downloaded")
+    await asyncio.sleep(delay)
+    print(f"{name} finished downloading")
+
+async def main():
+    task1 = asyncio.create_task(get_image("photo 1", 2))
+    task2 = asyncio.create_task(get_image("photo 2", 3))
+
+    await task1
+    await task2
+
+asyncio.run(main())
