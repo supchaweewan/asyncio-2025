2d7eac3fd04cdde207a84fd5450c6c1fbad3ef44
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:37:52 +0700

    completed asyncio12 assignment 4

--- assignment04/asyncio12.py
+++ assignment04/asyncio12.py
@@ -1,1 +1,11 @@
 # current task
+import asyncio
+
+async def show_current():
+    current = asyncio.current_task()
+    print("Task right now: ", current.get_name())
+
+async def main():
+    await show_current()
+
+asyncio.run(main())
