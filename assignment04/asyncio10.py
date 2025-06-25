0e0c05ba7e2693ac7d0e7e9f631649d5b1eb4aed
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:37:36 +0700

    completed asyncio10 assignment 4

--- assignment04/asyncio10.py
+++ assignment04/asyncio10.py
@@ -1,1 +1,16 @@
 # call back task
+import asyncio
+
+async def simple_task():
+    await asyncio.sleep(1)
+    return "we finished!"
+
+def on_done(task):
+    print("Callback: ", task.result())
+
+async def main():
+    task = asyncio.create_task(simple_task())
+    task.add_done_callback(on_done)
+    await task
+
+asyncio.run(main())
