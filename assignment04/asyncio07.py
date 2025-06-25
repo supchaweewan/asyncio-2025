cbde7f1c1f46e4ec917eea7f9c2e8bff36494900
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:37:12 +0700

    completed asyncio7 assignment 4

--- assignment04/asyncio07.py
+++ assignment04/asyncio07.py
@@ -1,1 +1,13 @@
 # get result
+import asyncio
+
+async def simple_task():
+    await asyncio.sleep(1)
+    return "We loaded dawg."
+
+async def main():
+    task = asyncio.create_task(simple_task())
+    await task
+    print("Result of this damn task:", task.result())
+
+asyncio.run(main())
