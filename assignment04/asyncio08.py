5e569efb397232961563a89b6df954c15cb857ca
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:37:19 +0700

    completed asyncio8 assignment 4

--- assignment04/asyncio08.py
+++ assignment04/asyncio08.py
@@ -1,1 +1,15 @@
 # get Exception
+import asyncio
+
+async def error_task():
+    await asyncio.sleep(1)
+    raise ValueError("Error happened.")
+
+async def main():
+    task = asyncio.create_task(error_task())
+    try:
+        await task
+    except Exception:
+        print("Exception that happened:", task.exception())
+
+asyncio.run(main())
