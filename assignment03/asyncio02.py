74945d9fe93929b1d051023c97c4929acd9b2a62
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:02:54 +0700

    Completed asyncio2 assignment 3

--- assignment03/asyncio02.py
+++ assignment03/asyncio02.py
@@ -1,2 +1,12 @@
 # example of running a coroutine
+import asyncio
 
+async def custom_coro():
+    
+    await asyncio.sleep(1)
+
+async def main():
+
+    await custom_coro()
+
+asyncio.run(main())
