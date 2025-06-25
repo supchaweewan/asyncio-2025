6b4fd6e911e908aafa35571c2a9162d22ed56c14
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:03:11 +0700

    completed asyncio03 assignment 3

--- assignment03/asyncio03.py
+++ assignment03/asyncio03.py
@@ -1,1 +1,17 @@
 # example of creating an event loop
+import asyncio
+
+async def say_after(delay, msg):
+    
+    await asyncio.sleep(delay)
+    print(msg)
+
+async def main():
+    task1 = asyncio.create_task(say_after(1, "hello"))
+    task2 = asyncio.create_task(say_after(2, "world"))
+
+    await task1
+    await task2
+
+
+asyncio.run(main())
