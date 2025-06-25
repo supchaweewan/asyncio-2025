aa0a500fb9b8dc2671f0d884bcef8fd7adf21c03
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-06-25 11:07:30 +0700

    completed asyncio02 assignment 4

--- assignment04/asyncio02.py
+++ assignment04/asyncio02.py
@@ -1,1 +1,13 @@
 # Create 1 Task with High-Level API
+import asyncio
+
+async def do_stuff():
+    print("working on it..")
+    await asyncio.sleep(2)
+    print("worked on it.")
+
+async def main():
+    task = asyncio.create_task(do_stuff())
+    await task
+
+asyncio.run(main())
