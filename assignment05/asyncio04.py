c79424de55fe0ca10971687a7c8b7e2a57db5a8e
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-07-02 10:35:19 +0700

    Completed asyncio4 of assignment 5

--- assignment05/asyncio04.py
+++ assignment05/asyncio04.py
@@ -1,1 +1,16 @@
 # example of waiting for all tasks to be completed with a timeout
+from random import random
+import asyncio
+
+async def task_coro(arg):
+    value = random()
+    await asyncio.sleep(value)
+    print(f'>task {arg} is done with {value}')
+    return f"task {arg} with {value}"
+
+async def main():
+    tasks = [asyncio.create_task(task_coro(i)) for i in range(10)]
+    done,pending = await asyncio.wait(tasks, timeout=0.5)
+    print(f'Done, {len(done)} takss completed in time')
+
+asyncio.run(main())
