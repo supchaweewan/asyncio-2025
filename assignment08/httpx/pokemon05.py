f9767db500d4ad0598539b4fba5717adcb25ab3c
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-07-16 10:42:43 +0700

    Added comments to make sure you understand why I'm using lambda for this.

--- assignment08/httpx/pokemon05.py
+++ assignment08/httpx/pokemon05.py
@@ -18,6 +18,10 @@ async def main():
 
     get_pokemon = [fetch(name) for name in pokemon_names]
     results = await asyncio.gather(*get_pokemon)
+    # Use lambda to define what value to call. Here it's used to call results[2]
+    # results[2] is the value where base_xp is
+    # Therefore we're using lambda as the key to call results[2], which is base_xp, to use in sorting
+    # reverse = True is to allow it to sort in descending order
     sorted_results = sorted(results, key=lambda results: results[2], reverse= True)
     
     for name, id, base_xp in sorted_results:
