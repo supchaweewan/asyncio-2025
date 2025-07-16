74d2f64c9cb117c4eace6d3c628da71afde3f087
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-07-16 10:28:07 +0700

    Coded pokemon 4 of assignment 8

---
+++ assignment08/httpx/pokemon04.py
@@ -0,0 +1,27 @@
+import asyncio
+import httpx
+
+async def fetch(name):
+    async with httpx.AsyncClient() as client:
+        url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+        response = await client.get(url)
+        data = response.json()
+        return name, data['id'], [t['type']['name'] for t in data['types']]
+
+    
+
+
+
+async def main():
+    pokemon_names = ["pikachu", "bulbasaur", "charmander", "squirtle", "eevee", "snorlax", "gengar", "mewtwo", "psyduck", "jigglypuff"]
+
+
+    get_pokemon = [fetch(name) for name in pokemon_names]
+    results = await asyncio.gather(*get_pokemon)
+    for name, id, types in results:
+        print(f"{name} -> ID: {id}, Type(s): {types}")
+
+
+asyncio.run(main())
+
+    
