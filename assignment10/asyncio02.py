e0acfdf24f4074e7ea0cb30b8a45338cf8c1823b
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-08-27 11:43:37 +0700

    finished async02 assignment10

--- assignment10/asyncio02.py
+++ assignment10/asyncio02.py
@@ -1,3 +1,36 @@
 # example of using an asyncio queue without blocking
 from random import random
 import asyncio
+
+async def producer(queue):
+    print('Producer: Running')
+    for i in range(10):
+        value = i
+        sleeptime = random()
+        print(f"> Producer {value} sleep {sleeptime}")
+        await asyncio.sleep(sleeptime)
+        print(f"> Producer put {value}")
+        await queue.put(value)
+    
+    await queue.put(None)
+    print('Producer: Done')
+
+async def consumer(queue):
+    print('Consumer: Running')
+    while True:
+        try:
+            item = queue.get_nowait()
+        except asyncio.QueueEmpty:
+            print('Consumer: got nothing. waiting for a bit')
+            await asyncio.sleep(0.5)
+            continue
+        if item is None:
+            break
+        print(f'\t> Consumer got {item} ')
+    print('Consumer: Done')
+
+async def main():
+    queue = asyncio.Queue()
+    await asyncio.gather(producer(queue), consumer(queue))
+
+asyncio.run(main())
