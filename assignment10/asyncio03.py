09a3cc79229d37ac91b2d758bc9afc68b3ce7e11
Suppanat Chaweewan <supchaweewan@gmail.com>
2025-08-27 11:43:50 +0700

    finished async03 assignment 10

--- assignment10/asyncio03.py
+++ assignment10/asyncio03.py
@@ -1,3 +1,28 @@
 from random import random
 import asyncio
 import time
+
+async def producer(queue):
+    print(f"{time.ctime()} Producer: Running")
+    for i in range(10):
+        value = random()
+        await asyncio.sleep(value)
+        await queue.put(value)
+
+    print(f"{time.ctime()} Producer: Done")
+
+async def consumer(queue):
+    print(f"{time.ctime()} Consumer: Running")
+    while True:
+        item = await queue.get()
+        print(f"{time.ctime()} >get {item}")
+        if item:
+            await asyncio.sleep(item)
+        queue.task_done()
+async def main():
+    queue = asyncio.Queue()
+    consumer_tasks = asyncio.create_task(consumer(queue))
+    await asyncio.create_task(producer(queue))
+    await queue.join()
+
+asyncio.run(main())
